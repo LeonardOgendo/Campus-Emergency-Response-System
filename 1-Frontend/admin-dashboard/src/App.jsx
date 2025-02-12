c305ff0d3fecb3c9931af1d7e9a9ba6de577f5ff
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-02-12 10:08:02 +0300

    Initial setup

---
+++ 1-Frontend/admin-dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+    return(
+        <div>
+            <h4>Admin Dashboard</h4>
+            <p>... in development</p>
+        </div>
+    )
+}
+
+export default App
---
+++ 1-Frontend/user-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+    return(
+        <>
+            <h4>User App</h4>
+            <p>...in development</p>
+        </>
+    )
+}
+
+export default App

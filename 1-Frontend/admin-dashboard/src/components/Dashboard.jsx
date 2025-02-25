bc13256d2f297239aa29c838500c4b319267e2ad
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-02-25 10:27:10 +0300

    additional styling

--- 1-Frontend/admin-dashboard/src/components/Dashboard.jsx
+++ 1-Frontend/admin-dashboard/src/components/Dashboard.jsx
@@ -34,36 +34,37 @@ const Dashboard = () => {
                 </div>
             </div>
 
-            <div className='header p-2 mt-5 rounded'>
+            <div className='header p-2 mt-5'>
                 <span className='fw-bold'>Most Student Occupancy Areas</span>
             </div>
             
-            <div className="occ-area-map mt-3 mb-3">
+            <div className="occ-area-map mt-1 mb-3">
                 <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31918.130563746294!2d34.72617621083985!3d0.29252480000002135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x17803c2381e6b747%3A0xd788ea540e29f427!2sMasinde%20Muliro%20University%20of%20Science%20and%20Technology!5e0!3m2!1sen!2ske!4v1740366870825!5m2!1sen!2ske" 
                         width="100%" 
-                        height="400" 
-                        style={{ 'border': '0', 'borderRadius': '10px'}}
+                        height="400px" 
+                        style={{ 'border': '0'}}
                         loading="lazy" 
                         referrerpolicy="no-referrer-when-downgrade"
                 ></iframe>
+                <div className="overlay"></div>
             </div>
             
             <div className="occupied-areas">
-                <div className="border py-4 px-3 rounded">
-                    <span>Name: </span><span>Lurambi</span><br></br>
+                <div className="py-4 px-3 rounded">
+                    <span className='name-label'>Name: </span><span className='area-name'>Lurambi</span><br></br>
                     <span>Population: </span><span>3,000</span>    
                 </div>
-                <div className="border py-4 px-3 rounded">
-                    <span>Name: </span><span>Sichirayi</span><br></br>
+                <div className="py-4 px-3 rounded">
+                    <span className='name-label'>Name: </span><span className='area-name'>Sichirayi</span><br></br>
                     <span>Population: </span><span>5,000</span>
                 </div>
-                <div className="border py-4 px-3 rounded">
-                    <span>Name: </span><span>Koromatangi</span><br></br>
+                <div className="py-4 px-3 rounded">
+                    <span className='name-label'>Name: </span><span className='area-name'>Koromatangi</span><br></br>
                     <span>Population: </span><span>5,000</span>
                 </div>
-                <div className="border py-4 px-3 rounded">
-                    <span>Name: </span><span>Kefinco</span><br></br>
+                <div className="py-4 px-3 rounded">
+                    <span className='name-label'>Name: </span><span className='area-name'>Kefinco</span><br></br>
                     <span>Population: </span><span>3, 260</span>
                 </div>
             </div>    

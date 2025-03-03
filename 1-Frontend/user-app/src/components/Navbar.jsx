f07882dc5a67b3ebbc90653bf0652a0b6e731aae
wambuamutuku <mutukuwambua400@gmail.com>
2025-03-03 12:46:08 +0300

    fixing the slider and navigation

--- 1-Frontend/user-app/src/components/Navbar.jsx
+++ 1-Frontend/user-app/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import '../styles.css';
+import Logo from '../assets/cers_logox2.png';
+import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return(
 
     <div className="container">
-
         <nav className="navbar navbar-expand-md ">
-            <img src="" alt="image" />
+             <div className="brand ps-3">
+                 <img id='logo' src={Logo} alt="CERS_Logo" />
+                 <span><a href="#" className='nav-brand text-decoration-none fw-bold fs-5 text-dark '>CERS</a></span>
+            </div>
+            
 
             <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span><i className='fa fa-bars'></i></span>
@@ -14,18 +19,18 @@ export default function Navbar(){
 
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Notifications</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Account</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Contact Us</a>
-                    </li>
+                <li className="nav-item">
+                    <Link to="/home" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/notification" className="nav-link">Notifications</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/account" className="nav-link">Account</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/contact" className="nav-link">Contact Us</Link>
+                </li>
                     <button className="btn-custom">
                         <a href="#" className="logout">Logout</a>
                     </button>
--- 1-Frontend/user-app/src/components/Slider.jsx
+++ 1-Frontend/user-app/src/components/Slider.jsx
@@ -9,44 +9,27 @@ export default function Slider(){
     <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    
   </div> 
 
     <div class="carousel-inner">
 
         <div class="carousel-item active c-item">
-        <img src="./src/assets/em2.jpeg" class="d-block w-100  c-image" alt="..."/>
+        <img src="./src/assets/1.png" class="d-block w-100  c-image" alt="..."/>
         <div class="carousel-caption top-0 mt-4 ">
-        <p className="mt-5 fs-3 text-uppercase">Some representative placeholder content for the first slide.</p>
-        <h1 className="display-1 fw-bolder text-capitalize">First slide label</h1>
-        <button className="btn btn-danger px-4 py-2 fs-5 mt-5 ">Report emergency</button>
         </div>   
         </div>
 
         <div class="carousel-item c-item">
-        <img src="./src/assets/em4.jpeg" class="d-block w-100 c-image" alt="..."/>
+        <img src="./src/assets/4.png" class="d-block w-100 c-image" alt="..."/>
         <div class="carousel-caption top-0 mt-4  ">
-        <p className="mt-5 fs-3 text-uppercase">Some representative placeholder content for the first slide.</p>
-        <h1 className="display-1 fw-bolder text-capitalize">First slide label</h1>
-        <button className="btn btn-danger px-4 py-2 fs-5 mt-5 ">Report emergency</button>
+      
         </div> 
         </div>
 
         <div class="carousel-item  c-item">
-        <img src="./src/assets/em5.jpeg" class="d-block w-100 c-image" alt="..."/>
+        <img src="./src/assets/3.png" class="d-block w-100 c-image" alt="..."/>
         <div class="carousel-caption top-0 mt-4  ">
-        <p className="mt-5 fs-3 text-uppercase">Some representative placeholder content for the first slide.</p>
-        <h1 className="display-1 fw-bolder text-capitalize">First slide label</h1>
-        <button className="btn btn-danger px-4 py-2 fs-5 mt-5 ">Report emergency</button>
-        </div> 
-        </div>
-
-        <div class="carousel-item c-item">
-        <img src="./src/assets/em3.jpeg" class="d-block w-100 c-image" alt="..."/>
-        <div class="carousel-caption top-0 mt-4   d-md-block">
-        <p className="mt-5 fs-3 text-uppercase">Some representative placeholder content for the first slide.</p>
-        <h1 className="display-1 fw-bolder text-capitalize">First slide label</h1>
-        <button className="btn btn-danger px-4 py-2 fs-5 mt-5 ">Report emergency</button>
         </div> 
         </div>
 

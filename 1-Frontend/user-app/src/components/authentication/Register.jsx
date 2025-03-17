242e46d8558d22234908cf326c8dab86b2abff2a
wambuamutuku <mutukuwambua400@gmail.com>
2025-03-17 08:41:16 +0300

    modifying register form

--- 1-Frontend/user-app/src/components/authentication/Register.jsx
+++ 1-Frontend/user-app/src/components/authentication/Register.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Register(){
     return(
 
@@ -5,8 +7,17 @@ export default function Register(){
         <div className='card text-black m-5' style={{ borderRadius: '25px' }}>
           <div className='card-body'>
             <div className='row'>
+
+            <div className='col-md-10 col-lg-6 order-1 order-lg-2 d-flex align-items-center'>
+                <img 
+                  src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' 
+                  className='img-fluid' 
+                  alt='Sign Up'
+                />
+              </div>
+
               <div className='col-md-10 col-lg-6 order-2 order-lg-1 d-flex flex-column align-items-center'>
-                <p className='text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4'>Sign up</p>
+                <p className='text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 link-danger'>Sign up</p>
                 
                 <div className='mb-4 w-100'>
                   <label className='form-label'>first_name</label>
@@ -33,19 +44,13 @@ export default function Register(){
                   <input type='password' className='form-control' />
                 </div>
   
-                <button className='btn btn-primary mb-4'>Register</button>
+                <button className="btn custom-btn px-5 mb-3 btn-danger" style={{width: "100%"}}>Sign up</button>
                 <p className="small fw-bold mt-2 pt-1 mb-2" >
-              Have have an account? <a href="#!" className="link-danger">Login</a>
+              Have have an account? <a href="#" className="link-danger"><Link to="/">login</Link></a>
             </p>
               </div>
   
-              <div className='col-md-10 col-lg-6 order-1 order-lg-2 d-flex align-items-center'>
-                <img 
-                  src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' 
-                  className='img-fluid' 
-                  alt='Sign Up'
-                />
-              </div>
+          
             </div>
           </div>
         </div>

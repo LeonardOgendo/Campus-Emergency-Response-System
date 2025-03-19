a83603201a0a881438b39b9d924fbac024b06f1c
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-03-19 22:00:54 +0300

    Fix code duplication error

--- 1-Frontend/user-app/src/components/pages/Content.jsx
+++ 1-Frontend/user-app/src/components/pages/Content.jsx
@@ -6,28 +6,23 @@ export default function Content() {
     const navigate = useNavigate();
 
     return(
-        <>
-            <div className="dash-container">
-                
-                <div className='box-1'>
-                    <div onClick={() => navigate('emergency/report')} className='p-3 rounded'>
-                    <div onClick={() => navigate('emergency/report')} className='p-3 rounded'>
-                        <p className='fw-bold'>Report Emergency</p>
-                    </div>
-                    <div className='p-3 rounded'>
-                        <p className='fw-bold'>Track Response status</p>
-                    </div>
-                    <div className='p-3 rounded'>
-                        <h5></h5>
-                        <p className="fw-bold">View Flagged Area</p>
-                    </div>
-                    <div className='p-3 rounded'>
-                        <p className="fw-bold">Incident History</p>
-                    </div>
+        <div className="dash-container">
+            
+            <div className='box-1'>
+                <div onClick={() => navigate('emergency/report')} className='p-3 rounded'>
+                    <p className='fw-bold'>Report Emergency</p>
                 </div>
-                
+                <div className='p-3 rounded'>
+                    <p className='fw-bold'>Track Response status</p>
+                </div>
+                <div className='p-3 rounded'>
+                    <h5></h5>
+                    <p className="fw-bold">View Flagged Area</p>
+                </div>
+                <div className='p-3 rounded'>
+                    <p className="fw-bold">Incident History</p>
+                </div>
             </div>
-       
-    </>
+        </div>   
     )
 }
